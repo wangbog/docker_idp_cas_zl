a78b71878a3f13a68731b880def1b918575e8cda
wangbo <wb626@pku.edu.cn>
2020-01-06 18:44:18 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# docker_idp_cas_zl
+# docker_idp_cas
+1. 配置宿主机环境
+
+配置本机IP
+
+修改主机域名 vi /etc/hostname 及 hostname命令
+
+关闭SELinux
+
+开放本机端口（如firewall-cmd）
+
+设置时间同步（NTP配置）
+
+2. 配置Docker环境
+
+sudo yum -y install -y yum-utils device-mapper-persistent-data lvm2
+
+sudo yum -y install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+
+可以用hello-world docker镜像验证一下环境（这一步可以不做）： sudo docker run hello-world
+
+git clone https://github.com/carsi-cernet/docker_idp_cas.git
+
+cd docker_idp_cas
+
+docker build --rm -t local/carsi-idp-cas .
+
+docker run -itd -v /opt/shibboleth-idp/:/opt/shibboleth-idp/ -v /etc/localtime:/etc/localtime:ro -v /etc/hostname:/etc/hostname -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 80:80 -p 443:443 -p 8443:8443 --privileged=true local/carsi-idp-cas
+
+查询container_id:
+
+docker ps -a
+
+进入该镜像的bash环境：
+
+docker exec -it <container_id> /bin/bash
+
+如需停止容器：
+
+docker stop <container_id> - 停止docker daemon
+
+3. 在容器bash内执行
+
+首先确认: hostname命令确认一下域名是否已识别；date命令确认一下时间及时区是否正确。
+
+修改/root/inst/idp3config/autoconfig.sh中的admin_mail为运维管理员的邮箱地址（此处是在Let's Encrypt申请网站证书时使用的）。
+
+sh /root/inst/idp3config/autoconfig.sh  （注意执行中需要输入idp域名，并多次输入证书密码。另外改配置自动回生成Let's Encript 证书。最后重新build的时候需要确认一下安装路径 Installation Directory: [/opt/shibboleth-idp]，此时需要回车确认）
+
+根据本校CAS的实际配置，修改/opt/shibboleth-idp/conf/路径下的 idp.properties 及 attribute-resolver.xml两个文件， 修改完毕后重启tomcat：systemctl status tomcat， 重启后访问一下https://<idp域名>/idp/ 应该可以看到“No services are available at this location.”的提示。（备注：本docker容器已自动配置为跳转到北大CARSI预上线测试用CAS服务器，因此理论上这一步不做配置也可以访问通，可先基于此配置验证环境是否妥当，之后再根据本校情况进行修改）
+
+将/opt/shibboleth-idp/metadata/idp-metadata.xml通过CARSI自服务系统上传到联盟（docker容器启动时已与宿主机同步了/opt/shibboleth-idp/路径，因此直接在宿主机中即可找到该文件）
+
+sh /root/inst/idp3config/startidp.sh
+
+之后即可在预上线环境中进行测试了。
---
+++ idp3config/autoconfig.sh
@@ -0,0 +1,69 @@
+source /etc/profile
+
+# config httpd
+\cp -f /root/inst/idp3config/httpd.conf /etc/httpd/conf/httpd.conf
+\cp -f /root/inst/idp3config/index.html /var/www/html/index.html
+\cp -f /root/inst/idp3config/ports.conf /etc/httpd/conf.d/ports.conf
+\cp -f /root/inst/idp3config/idp80.conf /etc/httpd/conf.d/idp80.conf
+systemctl restart httpd
+
+# config tomcat
+\cp -f /root/inst/idp3config/idp.xml /etc/tomcat/Catalina/localhost/idp.xml
+\cp -f /root/inst/idp3config/server.xml /etc/tomcat/server.xml
+\cp -f /root/inst/idp3config/javax.servlet.jsp.jstl-api-1.2.1.jar /usr/share/tomcat/lib/javax.servlet.jsp.jstl-api-1.2.1.jar
+\cp -f /root/inst/idp3config/javax.servlet.jsp.jstl-1.2.1.jar /usr/share/tomcat/lib/javax.servlet.jsp.jstl-1.2.1.jar
+systemctl restart tomcat
+
+# config https
+hostname=`hostname`
+admin_mail="admin@xxx.edu.cn"
+certbot certonly --agree-tos --no-eff-email -m $admin_mail -d $hostname --webroot -w /var/www/html
+systemctl restart crond
+echo "0 0 1 * * certbot renew >/dev/null 2>&1" >> /var/spool/cron/root
+\cp -f /root/inst/idp3config/ssl.conf /etc/httpd/conf.d/ssl.conf
+\cp -f /root/inst/idp3config/idp.conf /etc/httpd/conf.d/idp.conf
+sed -i "s/MY_IDP_HOSTNAME/$hostname/g" /etc/httpd/conf.d/idp.conf
+sed -i "s/#Listen 443/Listen 443/g" /etc/httpd/conf.d/ports.conf
+systemctl restart httpd
+
+# config idp
+rm -rf /root/inst/idp3config/shibboleth-identity-provider-3.4.3/
+cd /root/inst/idp3config/
+tar xzf shibboleth-identity-provider-3.4.3.tar.gz
+sh /root/inst/idp3config/shibboleth-identity-provider-3.4.3/bin/install.sh
+sed -i "s/Fill in the details for your IdP here/Fill in the details for your CAS IdP here/g" /opt/shibboleth-idp/metadata/idp-metadata.xml
+openssl pkcs12 -in /opt/shibboleth-idp/credentials/idp-backchannel.p12 -out /opt/shibboleth-idp/credentials/idp-backchannel.key -nocerts -nodes
+\cp -f /root/inst/idp3config/metadata-providers-pre.xml /opt/shibboleth-idp/conf/metadata-providers.xml
+\cp -f /root/inst/idp3config/attribute-resolver.xml /opt/shibboleth-idp/conf/attribute-resolver.xml
+salt=`openssl rand 32 -base64`
+sed -i "s/xxxxxxxxxxxxxxxxxxxx/$salt/g" /opt/shibboleth-idp/conf/attribute-resolver.xml
+\cp -f /root/inst/idp3config/audit.xml /opt/shibboleth-idp/conf/audit.xml
+\cp -f /root/inst/idp3config/consent-intercept-config.xml /opt/shibboleth-idp/conf/intercept/consent-intercept-config.xml
+\cp -f /root/inst/idp3config/relying-party.xml /opt/shibboleth-idp/conf/relying-party.xml
+\cp -f /root/inst/idp3config/attribute-filter.xml /opt/shibboleth-idp/conf/attribute-filter.xml
+\cp -f /root/inst/idp3config/dsmeta.pem /opt/shibboleth-idp/credentials/
+sed -i "s/#idp.consent.allowPerAttribute = false/idp.consent.allowPerAttribute = true/g" /opt/shibboleth-idp/conf/idp.properties
+wget -P /opt/shibboleth-idp/metadata/ https://dspre.carsi.edu.cn/carsifed-metadata-pre.xml
+chown -R tomcat.tomcat /opt/shibboleth-idp
+systemctl restart httpd
+systemctl restart tomcat
+
+# config CAS
+mkdir -p /opt/shibboleth-idp/flows/authn/Shibcas/
+\cp -f /root/inst/idp3config/shibcas-authn-beans.xml /opt/shibboleth-idp/flows/authn/Shibcas/shibcas-authn-beans.xml
+\cp -f /root/inst/idp3config/shibcas-authn-flow.xml /opt/shibboleth-idp/flows/authn/Shibcas/shibcas-authn-flow.xml
+\cp -f /root/inst/idp3config/no-conversation-state.jsp /opt/shibboleth-idp/edit-webapp/no-conversation-state.jsp
+\cp -f /root/inst/idp3config/shib-cas-authenticator-3.2.3.jar /opt/shibboleth-idp/edit-webapp/WEB-INF/lib/shib-cas-authenticator-3.2.3.jar
+\cp -f /root/inst/idp3config/cas-client-core-3.4.1.jar /opt/shibboleth-idp/edit-webapp/WEB-INF/lib/cas-client-core-3.4.1.jar
+\cp -f /root/inst/idp3config/web.xml /opt/shibboleth-idp/edit-webapp/WEB-INF/web.xml
+sed -i "s/idp.authn.flows=Password/idp.authn.flows=Shibcas/g" /opt/shibboleth-idp/conf/idp.properties
+echo "shibcas.casServerUrlPrefix = https://ssoedu.cn" >> /opt/shibboleth-idp/conf/idp.properties
+echo 'shibcas.casServerLoginUrl = ${shibcas.casServerUrlPrefix}/login' >> /opt/shibboleth-idp/conf/idp.properties
+echo "shibcas.serverName = https://`hostname`" >> /opt/shibboleth-idp/conf/idp.properties
+\cp -f /root/inst/idp3config/general-authn.xml /opt/shibboleth-idp/conf/authn/general-authn.xml
+\cp -f /root/inst/idp3config/attribute-resolver-cas.xml /opt/shibboleth-idp/conf/attribute-resolver.xml
+salt=`openssl rand 32 -base64`
+sed -i "s/xxxxxxxxxxxxxxxxxxxx/$salt/g" /opt/shibboleth-idp/conf/attribute-resolver.xml
+chown -R tomcat.tomcat /opt/shibboleth-idp
+/opt/shibboleth-idp/bin/build.sh
+systemctl restart tomcat
